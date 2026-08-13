ed5f014d598e9220c2c129243e34fa8cb2edbf66
Ryuna888 <wolf.children.ame.and.yuki.12357@gmail.com>
2026-08-14 02:14:16 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="masterledger-homescreen-v1-2";
+const CACHE="masterledger-homescreen-v1-3";
 const APP_SHELL=["./manifest.webmanifest","./icon.svg"];
 
 self.addEventListener("install",event=>{
